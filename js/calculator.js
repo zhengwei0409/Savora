35cfc34272b8bd6b49632d16871537c6a9c1e04f
ZhengWei <zhengwei0409@gmail.com>
2025-05-02 15:52:46 +0800

    done calculator and learning part

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",() => {
+    let listArr = document.querySelectorAll(".sub-header div");
+
+    listArr.forEach(function(listItem) {
+        listItem.addEventListener("click", function() {
+            listArr.forEach((l) => l.classList.remove("active-title"));
+
+            this.classList.add("active-title");
+        })
+    })
+})
+
+
+function scrollToSection() {
+    document.querySelector(".section-2").scrollIntoView({ behavior: 'smooth' });
+}
