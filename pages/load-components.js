d2c9f49cfcdc9834579ed28e2594f4a1d3fe59ea
jze0725 <jze0725@gmail.com>
2025-05-02 23:49:59 +0800

    Update

--- pages/load-components.js
+++ pages/load-components.js
@@ -18,3 +18,23 @@ nameElement.textContent = userName || 'Guest';
 const userEmail = ''; // Assume fetched from DB
 const emailElement  = document.getElementById('useremail');
 emailElement.textContent = userEmail || 'No email is provided';
+
+const userOcc = ''; // Assume fetched from DB
+const occElement  = document.getElementById('user-Occ');
+occElement.textContent = userOcc || 'No occupation';
+
+const userIncome = ''; // Assume fetched from DB
+const incomeElement  = document.getElementById('user-Income');
+incomeElement.textContent = userIncome || 'No Income';
+
+const userCountry = ''; // Assume fetched from DB
+const countryElement  = document.getElementById('user-Country');
+countryElement.textContent = userCountry || 'Country not provided';
+
+const userGender = ''; // Assume fetched from DB
+const genderElement  = document.getElementById('user-Gender');
+genderElement.textContent = userCountry || 'Gender not provided';
+
+const userDOB = ''; // Assume fetched from DB
+const dobElement  = document.getElementById('user-DOB');
+dobElement.textContent = userDOB || 'Date of birth not provided';
