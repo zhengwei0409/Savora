78634cc79346cb92011851ae82a2cacec8669096
jze0725 <jze0725@gmail.com>
2025-05-02 22:57:45 +0800

    Update

--- pages/load-components.js
+++ pages/load-components.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById('header').innerHTML = data;
       });
 });
+
+const userImage = ''; // Assume fetched from DB
+const img = document.getElementById('user-image');
+img.src = userImage || '/images/person-circle.png';
+
+const userName = ''; // Assume fetched from DB
+const nameElement  = document.getElementById('username');
+nameElement.textContent = userName || 'Guest';
+
+const userEmail = ''; // Assume fetched from DB
+const emailElement  = document.getElementById('useremail');
+emailElement.textContent = userEmail || 'No email is provided';
