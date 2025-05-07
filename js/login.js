7de20b91e710bcf3eaaad0b3fe7505feea3af819
DYing04 <149658085+DYing04@users.noreply.github.com>
2025-05-08 00:41:40 +0800

    done login page and modified chatbot page

---
+++ js/login.js
@@ -0,0 +1,17 @@
+/**
+ * Toggles password visibility
+ */
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggleIcon');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    }
+}
